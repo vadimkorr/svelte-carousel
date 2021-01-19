b77e85cf74d2ea13bdacc63f2b6a6380ead48361
Vadim <korobeinikovv@mail.ru>
2021-01-19 22:12:12 +0300

    Add event handlers

--- src/ImageCarousel.svelte
+++ src/ImageCarousel.svelte
@@ -1,22 +1,42 @@
 <script>
+  import { onMount } from 'svelte'
   /**
    * Enable Next/Previos arrows
    */
-	export let arrows = true;
+  export let arrows = true;
+  
+  let contentContainerElement
+  let children
+  onMount(() => {
+    children = contentContainerElement.children
+  })
+
+  function handleLeftClick() {
+    // TODO: implement
+  }
+  function handleRightClick() {
+    // TODO: implement
+  }
 </script>
 
 <div class="main-container">
   {#if arrows}
   <div class="side-container">
-    <span class="clickable">&lt;</span>
+    <span
+      class="clickable"
+      on:click={handleLeftClick}
+    >&lt;</span>
   </div>
   {/if}
-  <div class="content-container">
+  <div class="content-container" bind:this={contentContainerElement}>
     <slot></slot>
   </div>
   {#if arrows}
   <div class="side-container">
-    <span class="clickable">&gt;</span>
+    <span
+      class="clickable"
+      on:click={handleRightClick}
+    >&gt;</span>
   </div>
   {/if}
 </div>
@@ -28,6 +48,7 @@
   }
   .content-container {
     flex: 1;
+    display: flex;
   }
   .side-container {
     background-color: cornflowerblue;
