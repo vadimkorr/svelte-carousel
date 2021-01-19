2ddf1ff1aefce87010a948802061eea8f8881597
Vadim <korobeinikovv@mail.ru>
2021-01-19 20:43:59 +0300

    Remove unused storybook examples

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -27,20 +27,3 @@ Primary.args = {
   primary: true,
   label: 'Button',
 };
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
