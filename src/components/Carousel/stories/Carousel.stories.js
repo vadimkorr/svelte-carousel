f14a41349d9aed1a780bc70816c7b1a9a2873186
Vadim <korobeinikovv@mail.ru>
2021-06-29 11:17:33 +0300

    Format stories js

--- src/components/Carousel/stories/Carousel.stories.js
+++ src/components/Carousel/stories/Carousel.stories.js
@@ -1,27 +1,28 @@
-import CarouselView from './CarouselView.svelte';
-import CarouselViewCustomDots from './CarouselViewCustomDots.svelte';
-import CarouselViewCustomArrows from './CarouselViewCustomArrows.svelte';
+import CarouselView from './CarouselView.svelte'
+import CarouselViewCustomDots from './CarouselViewCustomDots.svelte'
+import CarouselViewCustomArrows from './CarouselViewCustomArrows.svelte'
 
 export default {
   title: 'Carousel',
-  component: CarouselView
-};
+  component: CarouselView,
+}
 
 const Template = ({ ...args }) => ({
   Component: CarouselView,
-  props: args
-});
-export const Primary = Template.bind({});
+  props: args,
+})
+export const Primary = Template.bind({})
 
 const TemplateCustomDots = ({ ...args }) => ({
   Component: CarouselViewCustomDots,
-  props: args
-});
-export const WithCustomDots = TemplateCustomDots.bind({});
+  props: args,
+})
+export const WithCustomDots = TemplateCustomDots.bind({})
 
 const TemplateCustomArrows = ({ ...args }) => ({
   Component: CarouselViewCustomArrows,
-  props: args
-});
-export const WithCustomArrows = TemplateCustomArrows.bind({});
+  props: args,
+})
+export const WithCustomArrows = TemplateCustomArrows.bind({})
+
 
